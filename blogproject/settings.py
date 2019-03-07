3c05ee3b1f9356039d8a387047c6e3b56717c7ec
이혜민 <ihyemin@ihyemin-ui-MacBook-Pro.local>
2019-03-07 19:45:39 +0900

    second commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3jpe^ao32o$jnkgm^(!3l!ggqw#z@bl-5bii#@_7c7e4&a0ylq'
+# SECRET_KEY = '3jpe^ao32o$jnkgm^(!3l!ggqw#z@bl-5bii#@_7c7e4&a0ylq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3jpe^ao32o$jnkgm^(!3l!ggqw#z@bl-5bii#@_7c7e4&a0ylq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
